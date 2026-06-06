f146e779352eb9f2b523b86ca819da03ffec4576
BelinaPoh <belina_poh@hotmail.com>
2026-06-06 18:40:45 +0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,22 @@
-pass
+    """
+    Task 1
+    - Create a function that would swap the value of x and y using only x and y as variables.
+    - x and y must be numeric.
+    - Return -1 if x and y is not numeric, and
+    - print the swapped values if both x and y are numeric.
+    """
+def swap(x, y):
+  while x != int, float, complex and y != int, float, complex:
+    return -1
+    break;
+
+swap(x, y) 
+
+
+# Task 2
+# Invoke the function "swap" using the following scenarios:
+# - "Apple", 10
+# - 9, 17
+
+swap("Apple", 10)
+swap(9, 17)
